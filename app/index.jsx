c305d6644d85fb6c3914fe11a52aeefe6c617377
AlMartinez <alfonso.martinez@poly.com>
2024-05-01 13:51:43 -0600

    Adding untracked files

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-2xl font-pblack">Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <Link href="/home" style={{ color: 'red' }}>Go to Home</Link>
+        </View>
+    );
+}
